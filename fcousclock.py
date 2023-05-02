87ea37c0f4a5fcdebeb7b14e0a751c0d0a32cb54
caocjun <129967686+caocjun@users.noreply.github.com>
2023-05-02 23:10:26 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,14 +1,13 @@
 import time
 
 minutes = int(input("enter the number of minutes to focus:"))
-seconds = minutes * 60
+seconds = minutes * 10
 
 while seconds:
-    minutes, seconds = divmod(minutes, seconds)
-    timer = '{:02d}:{:02d}'.format(minutes, seconds)
+    mins, secs = divmod(minutes, seconds)
+    timer = '{:02d}:{:02d}'.format(mins, secs)
     print(time, end="\r")
-    time.sleep(60)
-    second -= 1
+    time.sleep(1)
 
     print("time，up！")
     
